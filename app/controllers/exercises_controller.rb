56ab88b90fab2ca0069d3eaa0e9497fda7db82be
Alex <alex@ubuntu.(none)>
2013-03-07 23:25:11 +0100

    little refactoring

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,21 +1,20 @@
 class ExercisesController < ApplicationController
 	
-	def new
-		@@lesson = Lesson.find(params[:lesson_id])
-	end
-
-	def create
-		@exercise = @@lesson.exercises.new(params[:exercise])
-		if @exercise.save
-			redirect_to controller: :programs, action: :index
-		else 
-			redirect_to action: :new
-		end
-	end
-
-	def edit
-	end
-
-	def destroy
-	end
+  def new
+  end
+  
+  def create
+    @exercise = Exercise.new(params[:exercise])
+    if @exercise.save
+      redirect_to controller: :programs, action: :index
+    else 
+      redirect_to action: :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
 end
