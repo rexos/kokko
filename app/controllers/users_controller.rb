ee9a2daae43e6ce61ac4446e1f46d7e80dd09adc
rakwaht <Martintonid@gmail.com>
2013-03-08 18:29:56 +0100

    ano

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+	def index
+	end
+
+	def list
+		@trainings = Program.all
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,13 @@ Kokkonut::Application.routes.draw do
   get "lessons/new" => "lessons#new"
   post "lessons/create" => "lessons#create"
   get "lessons/index" => "lessons#index"
+  get "lessons/show/:lesson_id" => "lessons#show"
   
   #exercise routes
   get "exercises/new" => "exercises#new"
   post "exercises/create" => "exercises#create"
   post "exercises/add_association" => "exercises#add_ex_to_lesson"
 
+  #users routes
+  get "users/list" => "users#list"
 end
