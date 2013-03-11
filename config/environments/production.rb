8e5babf375b04a37f96c099b96357dfad46261fe
Alex <alex@ubuntu.(none)>
2013-03-11 18:59:14 +0100

    retry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,14 @@ Kokkonut::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  	address:              'smtp.gmail.com',
-  	port:                 587,
-  	domain:               'baci.lindsaar.net',
-  	user_name:            '<username>',
-  	password:             '<password>',
-  	authentication:       'plain',
-  	enable_starttls_auto: true  }
-
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'baci.lindsaar.net',
+    user_name:            'dea.dormi',
+    password:             'diomerda',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
