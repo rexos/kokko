6368f54289bb099770874e3fc53d28099b8dcdb5
Alex <alex@ubuntu.(none)>
2013-03-11 19:28:22 +0100

    try mailing

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -3,7 +3,7 @@ class SignupMailer < ActionMailer::Base
 
   def signup_mail(user)
     @user = user
-    mail(:to => user.email, :subject => "Welcome to Kokko Gym!")
+    mail(:to => user.email, :subject => "Welcome to Kokko Gym #{@user.username.capitalize}!", :body => "Hey #{@user.username.capitalize}! What\s up?" )
   end
   
 end
