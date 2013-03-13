c71802f78b7e34446fad5027642aacab4d65a32d
Alex <alex@ubuntu.(none)>
2013-03-13 15:49:10 +0100

    added admin user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    admin = User.new(:username => "deadormi", :password => "diomerda", :password_confirmation => "diomerda", :email => "dea.dormi@gmail.com", :role => "admin")
+    admin.save
     current_user
   end
 end
