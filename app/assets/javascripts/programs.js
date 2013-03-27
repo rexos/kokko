7b26ab69a72a41c8ca2d54135c41aa09c65f17ba
rexos <alex.pellegrini@live.com>
2013-03-27 18:32:01 +0100

    retry fixing

--- app/assets/javascripts/programs.js
+++ app/assets/javascripts/programs.js
@@ -4,7 +4,7 @@
 //= require edit
 $(document).ready(function(){
     $(document).on('ajax:success', function(){
-	$('div.modal-body').append('<div class="alert alert-success center" style="width:90%"><h4><strong>Feedback ricevuto con successo!<br />Grazie mille per la collaborazione!</strong><h4></div>');
+	$('#feedback-modal').children('div.modal-body').append('<div class="alert alert-success center" style="width:90%"><h4><strong>Feedback ricevuto con successo!<br />Grazie mille per la collaborazione!</strong><h4></div>');
 	$('button.btn-success').fadeOut('slow',function(){
 	    $('div.modal-footer').prepend('<button id="close" class="btn btn-success btn-large" style="display:none"><i class="icon-ok"></i>&nbsp;&nbsp;Chiudi</button>');
 	    $('#close').fadeIn('slow');
