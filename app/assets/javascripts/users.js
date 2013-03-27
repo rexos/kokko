88b0a69c1bbda529a38ae456c1ab9ed0c3bcb2ac
Francesco Serra <afnecors@gmail.com>
2013-03-27 10:41:01 +0100

    js folder icon trick (a special thanks to rexos)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,3 +3,9 @@
 //= require twitter/bootstrap
 //= require edit
 
+$(document).ready(function(){
+	$('li').click(function(){
+		$('li.active').children('a').children('h4').children('i').toggleClass('icon-folder-close-alt icon-folder-open-alt');
+		$(this).children('a').children('h4').children('i').toggleClass('icon-folder-close-alt icon-folder-open-alt');
+	});
+});
