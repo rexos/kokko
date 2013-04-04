7693b607b350bc267ae20b895452f0c8f3f6bfc4
rexos <alex.pellegrini@live.com>
2013-04-04 17:16:10 +0200

    fixed?

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -1,7 +1,6 @@
 # -*- coding: undecided -*-
 class SignupMailer < ActionMailer::Base
   default from: "deadormi@info.com"
-  default body: "ciao stronzo"
 
   def signup_mail(user)
     @user = user
