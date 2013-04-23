3e3ea856b684e9c34d614cead73e2a8dfb7fe2b0
Francesco Serra <afnecors@gmail.com>
2013-04-23 11:40:33 +0200

    jquery arrow animation first user login

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	// la funzione fa rimbalzare la frecca quando l'utente 
+	// entra la prima volta nella app e deve ancora scegliere un training program
+	function bounce(x){
+		if (x==1) {
+			$('#bounce-arrow').animate({
+    		marginLeft: "+=40px"
+			}, 500);
+    		bounce(0);
+		}
+		else {
+			$('#bounce-arrow').animate({
+    		marginLeft: "-=40px"
+			}, 500);
+    		bounce(1);
+		}
+	}
+	bounce(1);
+
 	$('p.comments').click(function(){
 		$(this).next().slideToggle('fast');
 		$(this).children('i').toggleClass('icon-arrow-down icon-arrow-up');
