943cc44450bec193c33c3c295caffdaf93bd0ecd
rakwaht <Martintonid@gmail.com>
2013-05-08 14:07:06 +0200

    fix video and feedbacks

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	window.updateIframe = function() {
 	    var h = $(window).height();
-	    $("#iframe").height(0.5*h);
+	    $(".video").height(0.5*h);
 	}
 	window.updateIframe();
 	window.resize(window.updateIframe);
